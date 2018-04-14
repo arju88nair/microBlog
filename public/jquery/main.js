8c8278cc9261c903acb16f49c4e6b8931a4f801a
arju88nair <arju88nair@gmail.com>
2018-04-15 00:05:31 +0530

    cleaned JS file before pagination

--- public/jquery/main.js
+++ public/jquery/main.js
@@ -1,15 +1,64 @@
-$('.pp-filter-button').on('click', function (e) {
-    // remove btn-primary from all buttons first
-    $('.pp-filter-button').removeClass('btn-primary');
-    $('.pp-filter-button').addClass('btn-outline-primary');
 
-    // add btn-primary to active button
-    var button = $(this);
-    button.removeClass('btn-outline-primary');
-    button.addClass('btn-primary');
-    filterItems(button.data("filter"));
-    e.preventDefault();
-})
+$(document).ready(function()
+{
+    let category = "all";
+    // Pagination portion
+    let count = 10;
+
+    function loadMore() {
+        console.log("More loaded" + category);
+
+        $.ajax({
+
+            url: '/categoryscroll?page=' + count + "&cat=" + "{{category}}",
+            dataType: "json",
+            success: function (data) {
+                String.prototype.trunc = String.prototype.trunc ||
+                    function (n) {
+                        return (this.length > n) ? this.substr(0, n - 1) + '&hellip;' : this;
+                    };
+                data = JSON.parse(data);
+                count = count + 20;
+                $.each(data, function (index, value) {
+
+                });
+                $('#loading').hide();
+            }
+        });
+
+        $(window).bind('scroll', bindScroll);
+    }
+
+    function bindScroll() {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 1500) {
+            $(window).unbind('scroll');
+            loadMore();
+        }
+    }
+
+    $(window).scroll(bindScroll);
+
+
+// Category filtering
+
+    $('.pp-filter-button').on('click', function (e) {
+        // remove btn-primary from all buttons first
+        $('.pp-filter-button').removeClass('btn-primary');
+        $('.pp-filter-button').addClass('btn-outline-primary');
+
+        // add btn-primary to active button
+        let button = $(this);
+        button.removeClass('btn-outline-primary');
+        button.addClass('btn-primary');
+        category=button.data("filter");
+        filterItems(button.data("filter"));
+        e.preventDefault();
+    })
+
+});
+
+
+
 
 function filterItems(filter) {
     $.ajax({
@@ -20,7 +69,7 @@ function filterItems(filter) {
         },
         dataType: 'json',
         success: function (data) {
-            $(".alert-warning").hide()
+            $(".alert-warning").hide();
             let content = data.data;
             $('#data-div').html('');
             $.each(content, function (i, item) {
@@ -29,51 +78,14 @@ function filterItems(filter) {
 
             });
 
-            },
+        },
         error: function (request, error) {
-            $(".alert-warning").show()
+            $(".alert-warning").show();
 
         }
     });
 
 }
-
-
-var count = 10
-
-function loadMore() {
-    console.log("More loaded" + count);
-
-    $.ajax({
-
-        url: '/categoryscroll?page=' + count + "&cat=" + "{{category}}",
-        dataType: "json",
-        success: function (data) {
-            String.prototype.trunc = String.prototype.trunc ||
-                function (n) {
-                    return (this.length > n) ? this.substr(0, n - 1) + '&hellip;' : this;
-                };
-            data = JSON.parse(data)
-            count = count + 20
-            $.each(data, function (index, value) {
-
-            });
-            $('#loading').hide();
-        }
-    });
-
-    $(window).bind('scroll', bindScroll);
-}
-
-function bindScroll() {
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - 1500) {
-        $(window).unbind('scroll');
-        loadMore();
-    }
-}
-
-$(window).scroll(bindScroll);
-
 
 
 function populate(item)
